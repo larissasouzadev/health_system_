990e30c511ab2127969450fb7d94031c07415c24
larissa <larissacsouza585@gmail.com>
2025-05-31 23:30:31 -0300

    adding verbose

--- schedule/models.py
+++ schedule/models.py
@@ -4,16 +4,23 @@ from doctors.models import Doctors
 
 class Schedule(models.Model):
     id = models.AutoField(primary_key=True)
-    client = models.ForeignKey(Client, on_delete=models.CASCADE)
-    doctor = models.ForeignKey(Doctors, on_delete=models.CASCADE)
-    date = models.DateField()
-    hour = models.TimeField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    client = models.ForeignKey(Client, 
+                               on_delete=models.CASCADE,
+                               verbose_name='cliente')
+    doctor = models.ForeignKey(Doctors,
+                               on_delete=models.CASCADE,
+                               verbose_name='médico'
+                               )
+    date = models.DateField(verbose_name='data')
+    hour = models.TimeField(verbose_name='horário')
+    created_at = models.DateTimeField(auto_now_add=True,
+                                      verbose_name='criado em ')
+    updated_at = models.DateTimeField(auto_now_add=True,
+                                      verbose_name='atualizado em')
 
     class Meta:
         verbose_name = "Agenda"
-        verbose_name_plural = "Schedules"
+        verbose_name_plural = "Agenda"
 
     def __str__(self):
         return f"{self.client} - {self.doctor} - {self.date} {self.hour}"
