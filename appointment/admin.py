b620342ebfc0625ffe72e964b99616401974d9e9
larissa <larissacsouza585@gmail.com>
2025-06-04 11:43:14 -0300

    feature signals

--- appointment/admin.py
+++ appointment/admin.py
@@ -4,8 +4,9 @@ from appointment.models import Appointment
 
 @admin.register(Appointment)
 class AppointmentAdmin(admin.ModelAdmin):
-    list_display = ['name', 'appointment', 'doctor','speciliaties','date','created_at','updated_at', ]
-    search_fields = ['name', 'appointment']
+    list_display = ['name', 'available_schedule', 'doctor','speciliaties','date','created_at','updated_at', 'hour']
+    search_fields = ['name', 'doctor']
     list_filter = ['name', 'date']
-
-
+# desabilita os horários ocupados
+    def qs_list(self, request):
+        pass
--- appointment/signals.py
+++ appointment/signals.py
@@ -3,11 +3,21 @@ from django.dispatch import receiver
 from appointment.models import Appointment
 
 @receiver(post_save, sender=Appointment)
-def updated_available(sender, instance, created, **kwargs):
+def updated_available_status(sender, instance, created, **kwargs):
     """Atualiza automaticamente a disponibilidade do horário"""
-    if hasattr(instance, 'available_schedule') and instance.available_schedule:
-        instance.available_schedule.available = (instance.hour is None)
-        instance.available_schedule.save()
+    if created:  # Se for um novo agendamento
+        instance.available_schedule = False
+        instance.save()
+   
+   
+   
+   
+   
+   
+   
+    # if hasattr(instance, 'available_schedule') and instance.available_schedule:
+    #     instance.available_schedule.available = (instance.hour is None)
+    #     instance.available_schedule.save()
       
       
     #   arrumar signals e add hour a display list
